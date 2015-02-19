292e1a897ef1000823332b84186add55d12201ad
vladmihalcea <mih_vlad@yahoo.com>
2015-02-19 23:22:00 +0200

    Add more use cases

--- core/src/test/java/com/vladmihalcea/hibernate/masterclass/laboratory/concurrency/LockModePessimisticReadWriteIntegrationTest.java
+++ core/src/test/java/com/vladmihalcea/hibernate/masterclass/laboratory/concurrency/LockModePessimisticReadWriteIntegrationTest.java
@@ -53,6 +53,50 @@ public class LockModePessimisticReadWriteIntegrationTest extends AbstractIntegra
     }
 
     @Test
+    public void testPessimisticReadDoesNotBlockPessimisticRead() throws InterruptedException {
+        LOGGER.info("Test testPessimisticReadDoesNotBlockPessimisticRead");
+        doInTransaction(new TransactionCallable<Void>() {
+            @Override
+            public Void execute(Session session) {
+                try {
+                    Product product = (Product) session.get(Product.class, 1L);
+                    session.buildLockRequest(new LockOptions(LockMode.PESSIMISTIC_READ)).lock(product);
+                    LOGGER.info("PESSIMISTIC_READ acquired");
+
+                    executeNoWait(new Callable<Void>() {
+                        @Override
+                        public Void call() throws Exception {
+                            doInTransaction(new TransactionCallable<Void>() {
+                                @Override
+                                public Void execute(Session _session) {
+                                    awaitOnLatch(startLatch);
+                                    Product _product = (Product) _session.get(Product.class, 1L);
+                                    _session.buildLockRequest(new LockOptions(LockMode.PESSIMISTIC_READ)).lock(_product);
+                                    LOGGER.info("PESSIMISTIC_WRITE acquired");
+                                    return null;
+                                }
+                            });
+                            endLatch.countDown();
+                            return null;
+                        }
+                    });
+                    sleep(WAIT_MILLIS, new Callable<Void>() {
+                        @Override
+                        public Void call() throws Exception {
+                            startLatch.countDown();
+                            return null;
+                        }
+                    });
+                } catch (StaleObjectStateException expected) {
+                    LOGGER.info("Failure: ", expected);
+                }
+                return null;
+            }
+        });
+        awaitOnLatch(endLatch);
+    }
+
+    @Test
     public void testPessimisticReadBlocksPessimisticWrite() throws InterruptedException {
         LOGGER.info("Test PessimisticReadBlocksPessimisticWrite");
         doInTransaction(new TransactionCallable<Void>() {
@@ -73,6 +117,51 @@ public class LockModePessimisticReadWriteIntegrationTest extends AbstractIntegra
                                     Product _product = (Product) _session.get(Product.class, 1L);
                                     _session.buildLockRequest(new LockOptions(LockMode.PESSIMISTIC_WRITE)).lock(_product);
                                     LOGGER.info("PESSIMISTIC_WRITE acquired");
+                                    return null;
+                                }
+                            });
+                            endLatch.countDown();
+                            return null;
+                        }
+                    });
+                    sleep(WAIT_MILLIS, new Callable<Void>() {
+                        @Override
+                        public Void call() throws Exception {
+                            startLatch.countDown();
+                            return null;
+                        }
+                    });
+                } catch (StaleObjectStateException expected) {
+                    LOGGER.info("Failure: ", expected);
+                }
+                return null;
+            }
+        });
+        awaitOnLatch(endLatch);
+    }
+
+    @Test
+    public void testPessimisticReadBlocksUpdate() throws InterruptedException {
+        LOGGER.info("Test testPessimisticReadBlocksUpdate");
+        doInTransaction(new TransactionCallable<Void>() {
+            @Override
+            public Void execute(Session session) {
+                try {
+                    Product product = (Product) session.get(Product.class, 1L);
+                    session.buildLockRequest(new LockOptions(LockMode.PESSIMISTIC_READ)).lock(product);
+                    LOGGER.info("PESSIMISTIC_READ acquired");
+
+                    executeNoWait(new Callable<Void>() {
+                        @Override
+                        public Void call() throws Exception {
+                            doInTransaction(new TransactionCallable<Void>() {
+                                @Override
+                                public Void execute(Session _session) {
+                                    awaitOnLatch(startLatch);
+                                    Product _product = (Product) _session.get(Product.class, 1L);
+                                    _product.setDescription("USB Flash Memory Stick");
+                                    _session.flush();
+                                    LOGGER.info("Implicit lock acquired");
                                     return null;
                                 }
                             });
