5305d70b2c8dc83abf8d2f02e9a77b97ae98d5c2
vladmihalcea <mih_vlad@yahoo.com>
2015-03-20 08:41:07 +0200

    Code formatting

--- core/src/test/java/com/vladmihalcea/hibernate/masterclass/laboratory/entityidentifier/EntityIdentifierTest.java
+++ core/src/test/java/com/vladmihalcea/hibernate/masterclass/laboratory/entityidentifier/EntityIdentifierTest.java
@@ -1,7 +1,6 @@
 package com.vladmihalcea.hibernate.masterclass.laboratory.entityidentifier;
 
 import com.vladmihalcea.hibernate.masterclass.laboratory.util.AbstractTest;
-import org.hibernate.Session;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertSame;
@@ -19,31 +18,38 @@ public class EntityIdentifierTest extends AbstractTest {
 
     @Test
     public void testEntityIdentifier() {
-        doInTransaction(new TransactionCallable<Void>() {
-            @Override
-            public Void execute(Session session) {
-                Product product = new Product("LCD");
-                session.persist(product);
-                EntityEvent productEvent = new EntityEvent();
-                productEvent.setMessage(String.format("Product %s added", product.getName()));
-                productEvent.setEntityIdentifier(new EntityIdentifier(
-                        product.getClass(),
-                        product.getId()
-                ));
-                session.persist(productEvent);
-                EntityAttribute productAttribute = new EntityAttribute();
-                productAttribute.setName("AD_CAMPAIGN");
-                productAttribute.setValue("LCD_Sales");
-                productAttribute.setEntityIdentifier(new EntityIdentifier(
-                        product.getClass(),
-                        product.getId()
-                ));
-                session.persist(productAttribute);
-                assertSame(1, session.createQuery("select ea from EntityAttribute ea where ea.entityIdentifier = :entityIdentifier")
-                        .setParameter("entityIdentifier", new EntityIdentifier(product.getClass(), product.getId()))
-                        .list().size());
-                return null;
-            }
+        doInTransaction(session -> {
+            Product product = new Product("LCD");
+            session.persist(product);
+            EntityEvent productEvent = new EntityEvent();
+            productEvent.setMessage(String.format(
+                "Product %s added", product.getName()));
+            productEvent.setEntityIdentifier(
+                new EntityIdentifier(
+                    product.getClass(),
+                    product.getId()
+            ));
+            session.persist(productEvent);
+            EntityAttribute productAttribute =
+                new EntityAttribute();
+            productAttribute.setName("AD_CAMPAIGN");
+            productAttribute.setValue("LCD_Sales");
+            productAttribute.setEntityIdentifier(
+                new EntityIdentifier(
+                    product.getClass(),
+                    product.getId()
+            ));
+            session.persist(productAttribute);
+            assertSame(1, session.createQuery(
+                "select ea " +
+                "from EntityAttribute ea " +
+                "where " +
+                "   ea.entityIdentifier = :entityIdentifier")
+                .setParameter("entityIdentifier",
+                    new EntityIdentifier(
+                        product.getClass(), product.getId()))
+                .list().size());
+            return null;
         });
     }
 }
